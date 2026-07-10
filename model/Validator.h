1ed3e44b070d98cf756248450d1e18a19057030c
Andrei Cujo <andrei@razie.com>
2026-07-09 20:35:47 -0400

    just fixed some random merge conflict markers being left in the code from my laptop.

--- model/Validator.h
+++ model/Validator.h
@@ -19,12 +19,12 @@ enum class Error {
 
 class Validator {
     public:
-        expected<string, Error> validateRepoName           (const std::string &name);
-        expected<string, Error> validateRepoPath           (const std::string &path);
-        expected<string, Error> validateCommitID           (const std::string &id);
-        expected<string, Error> validateMessage            (const std::string &msg);
-        expected<string, Error> validateAuthor             (const std::string &author);
-        //std::optional<Error> validateStatusTransition   (std::string name); not sure what to do with this one yet
+        expected<string, Error> validateRepoName        (const std::string &name);
+        expected<string, Error> validateRepoPath        (const std::string &path);
+        expected<string, Error> validateCommitID        (const std::string &id);
+        expected<string, Error> validateMessage         (const std::string &msg);
+        expected<string, Error> validateAuthor          (const std::string &author);
+        //std::optional<Error> validateStatusTransition (const std::string name); not sure what to do with this one yet
 private:
     std::string trim (const::string &name);
 };
